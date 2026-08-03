cdc8b80cda5e363c5aca1cb1b6427bb3c5d67eb5
haroldmud <haroldmudosa@gmail.com>
2026-08-03 23:40:37 +0200

    feat: initial commit

---
+++ expense-tracker.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+
+
+
+namespace ExpenseTracker
+{
+  class MyExpense {
+    public record ExpenseType(string Expense, double Amount, string Currency, DateOnly Date, string? Description, bool IsPaid);
+    static void Main()
+    {
+       List<ExpenseType> Expenses = new List<ExpenseType>();
+      //  Expenses.Add(new("shoes", 25.4, "USD", DateOnly.FromDateTime(DateTime.Now), "nothin", true ));
+       AddExpense(Expenses);
+       Console.WriteLine(Expenses[0]);
+
+    }
+
+    static void AddExpense(List<ExpenseType> expensesArray)
+    {
+      Console.WriteLine("\n Add a New Expense \n \n \n \n");
+
+      Console.Write("Enter name: ");
+      string expense = Console.ReadLine() ?? "Unknown";
+
+      Console.Write("Enter amount in $: ");
+      string amount = Console.ReadLine() ?? "Unknown";
+      double parsedAmount = 0;
+      if(double.TryParse(amount, out double result))
+      {
+        parsedAmount = result;
+      } else
+      {
+        Console.WriteLine("The Amount is Unsupported, You will retry later");
+      }
+
+
+      string currency = "USD";
+      DateOnly entryDate = DateOnly.FromDateTime(DateTime.Now);
+
+      Console.Write("Enter Description: ");
+      string description = Console.ReadLine() ?? "No Description";
+
+      // Console.Write("Is Whatever purchased paid ? (Y/n) ");
+      bool isPaid = true;
+      // ConsoleKeyInfo keyInfo = Console.ReadKey(intercept: true);
+      // if(keyInfo.key == ConsoleKey.Y)
+      // {
+      //   Console.WriteLine("Yes");
+      //   isPaid = true;
+      // }
+      // if(keyInfo.Key == ConsoleKey.N)f
+      // {
+      //   Console.WriteLine("No");
+      //   isPaid = false;
+      // }
+
+      expensesArray.Add(new(expense, parsedAmount , currency, entryDate, description, isPaid));
+    }
+  }
+}
